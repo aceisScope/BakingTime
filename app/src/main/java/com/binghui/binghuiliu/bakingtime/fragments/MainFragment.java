a207e6de9718a35108bd8f4e40b150c01a6737b7
Binghui Liu <bertieliu52@gmail.com>
2017-09-26 15:51:51 +0300

    add butterknife dependency

--- app/src/main/java/com/binghui/binghuiliu/bakingtime/fragments/MainFragment.java
+++ app/src/main/java/com/binghui/binghuiliu/bakingtime/fragments/MainFragment.java
@@ -9,14 +9,19 @@ import android.view.ViewGroup;
 
 import com.binghui.binghuiliu.bakingtime.R;
 
+import butterknife.ButterKnife;
+
 /**
  * Created by binghuiliu on 25/09/2017.
  */
 
 public class MainFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_recipe_fragment, container, false);
+        View view = inflater.inflate(R.layout.main_recipe_fragment, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 }
