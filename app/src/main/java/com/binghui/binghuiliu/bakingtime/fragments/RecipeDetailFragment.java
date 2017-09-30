ac2710eeb880c25849132f1e1c15dcd5bd9ef14a
Binghui Liu <bertieliu52@gmail.com>
2017-09-30 12:38:41 +0300

    add recyler view to the recipe detail page

--- app/src/main/java/com/binghui/binghuiliu/bakingtime/fragments/RecipeDetailFragment.java
+++ app/src/main/java/com/binghui/binghuiliu/bakingtime/fragments/RecipeDetailFragment.java
@@ -6,9 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.binghui.binghuiliu.bakingtime.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -16,6 +18,10 @@ import butterknife.ButterKnife;
  */
 
 public class RecipeDetailFragment extends Fragment {
+
+    @BindView(R.id.text_ingredient)
+    TextView textIngredient;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
