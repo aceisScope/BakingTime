886a96c7ae2292a80bd6e6194f3f5c2fed993d27
Binghui Liu <bertieliu52@gmail.com>
2017-10-01 13:35:40 +0300

    setup exoplayer in recipe step

--- app/src/main/java/com/binghui/binghuiliu/bakingtime/RecipeListActivity.java
+++ app/src/main/java/com/binghui/binghuiliu/bakingtime/RecipeListActivity.java
@@ -2,13 +2,12 @@ package com.binghui.binghuiliu.bakingtime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 public class RecipeListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipe_list);
     }
 }
---
+++ app/src/main/java/com/binghui/binghuiliu/bakingtime/fragments/RecipeStepFragment.java
@@ -0,0 +1,79 @@
+package com.binghui.binghuiliu.bakingtime.fragments;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.binghui.binghuiliu.bakingtime.R;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.util.Util;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by binghuiliu on 01/10/2017.
+ */
+
+public class RecipeStepFragment extends Fragment {
+
+    @BindView(R.id.video_step)
+    SimpleExoPlayerView stepVideo;
+
+    private SimpleExoPlayer player;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.recipe_step_fragment, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        initializePlayer();
+    }
+
+    private void initializePlayer() {
+        stepVideo.requestFocus();
+
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        DataSource.Factory mediaDataSourceFactory = new DefaultDataSourceFactory(getContext(), Util.getUserAgent(getContext(), "mediaPlayerSample"), (TransferListener<? super DataSource>) bandwidthMeter);
+
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        player = ExoPlayerFactory.newSimpleInstance(getContext(), new DefaultTrackSelector(videoTrackSelectionFactory));
+
+        stepVideo.setPlayer(player);
+
+        player.setPlayWhenReady(false);
+        
+        DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        MediaSource mediaSource = new ExtractorMediaSource(Uri.parse("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"),
+                mediaDataSourceFactory, extractorsFactory, null, null);
+
+        player.prepare(mediaSource);
+    }
+}
