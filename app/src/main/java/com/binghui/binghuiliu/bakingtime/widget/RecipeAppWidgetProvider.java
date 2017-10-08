4a3e9a87210c70a52ca0e3a089e6b65b0d3a1b0a
Binghui Liu <bertieliu52@gmail.com>
2017-10-08 14:23:49 +0300

    add a widget

---
+++ app/src/main/java/com/binghui/binghuiliu/bakingtime/widget/RecipeAppWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.binghui.binghuiliu.bakingtime.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by binghuiliu on 2017/10/8.
+ */
+
+public class RecipeAppWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
